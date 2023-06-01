ab3d2f0c2b4ead3d27cda05b091f5b08a4c28c01
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-06-01 16:34:14 +0000

    did unt6-04

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Lamees Hemdan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates the volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get("radius")
+
+  // process
+  const dimensions = "Radius: " + radius + " units"
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+
+  // output
+  document.getElementById("dimensions").innerHTML = dimensions
+  document.getElementById("volume").innerHTML = "The volume is: " + volume.toFixed(2) + " units³"
+
+}
